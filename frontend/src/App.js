c301b60138f3c2af309d68962b18f0e358dcc103
Cerqueira <marcelo.galvao@live.com>
2020-04-11 16:57:01 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./App.css";
+import Route from './routes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Route/>
+    )
+  }
+}
---
+++ frontend/src/pages/Box/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from '../../assets/logo.svg';
+import { MdInsertDriveFile } from 'react-icons/md';
+import './styles.css';
+import api from '../../services/api';
+import { formatDistance, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR'
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client';
+
+export default class Box extends Component {
+    state = {
+        box:{}
+    }
+
+    async componentDidMount(){
+        this.subscribeToNewFiles();
+        const box = this.props.match.params.id;
+        const response = await api.get(`boxes/${box}`);
+
+        this.setState({ box: response.data});
+    }
+
+    subscribeToNewFiles = () => {
+        const box = this.props.match.params.id;
+        const io = socket('http://localhost:3000');
+        io.emit('connectRoom', box);
+        io.on("file",data => {
+           this.setState({ box: {...this.state.box, files: [data,...this.state.box.files] }});            
+        });
+    }
+     
+    handleUpload = (files) => {
+        files.forEach(file => {
+          const data = new FormData();
+          const box = this.props.match.params.id;
+          data.append('file',file);
+          api.post(`boxes/${box}/files`,data);
+            
+        });
+    };
+
+    render() {
+        return (
+            <div id="box-container">
+                <header>
+                    <img src={logo} alt="logo" />
+                    <h1>{ this.state.box.title }</h1>
+                </header>
+                <Dropzone onDropAccepted={ this.handleUpload }>
+                    {({ getRootProps, getInputProps}) => (
+                        <div className="upload" {...getRootProps()}>
+                            <input {...getInputProps() }/>
+                            <p>Arraste arquivos ou clique aqui! </p>
+                        </div>
+                    )}
+                </Dropzone>
+                <ul>
+                    { this.state.box.files && this.state.box.files.map(file =>(
+                        <li key = { file._id } >
+                            <a className="fileInfo" href={ file.url }>
+                                <MdInsertDriveFile size={24} color="#A5Cfff"/>
+                                <strong>{ file.title }</strong>
+                            </a>
+                            <span>
+                                Alterado há:{" "}
+                                {formatDistance(
+                                    parseISO(file.createdAt),
+                                    new Date(),
+                                    {locale:pt}
+                                )}
+                            </span>
+                        </li>    
+                    ))}    
+                </ul>
+
+            </div>
+        )
+    }
+}
